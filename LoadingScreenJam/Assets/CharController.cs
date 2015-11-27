399e43e4e21b064347fbbe5c4e9f33bc50b7a812
thecomediclife <thecomediclife324@gmail.com>
2015-11-27 03:06:07 -0800

    CharControllerComplete

---
+++ LoadingScreenJam/Assets/CharController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharController : MonoBehaviour {
+	public float speed = 5.0f;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		rb.velocity = new Vector2 (Input.GetAxis ("Horizontal") * speed, Input.GetAxis ("Vertical") * speed);
+	}
+}
